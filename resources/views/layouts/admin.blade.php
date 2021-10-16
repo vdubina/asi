2f03bc1586d6cdd2b97e8771c2f32c0a256d2bc0
Vladimir Dubina <vdubina@zgtec.com>
2021-10-16 14:06:19 +0300

    Merging QuickAdminPanel changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -108,9 +108,9 @@
 
         <footer class="main-footer">
             <div class="float-right d-none d-sm-block">
-                <b>Version</b> 3.0.0-alpha
+                <b>Version</b> 1.0-dev
             </div>
-            <strong> &copy;</strong> {{ trans('global.allRightsReserved') }}
+            <strong> &copy; {{ trans('panel.site_title') }}</strong> - {{ trans('global.allRightsReserved') }}
         </footer>
         <form id="logoutform" action="{{ route('logout') }}" method="POST" style="display: none;">
             {{ csrf_field() }}
