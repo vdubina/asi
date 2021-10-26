dd8c29c0a4d40ad252ef1feae76352fd8d2a34b0
Vladimir Dubina <vdubina@zgtec.com>
2021-10-26 15:48:54 +0300

    Fixing bugs

--- resources/views/admin/structures/reorder.blade.php
+++ resources/views/admin/structures/reorder.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.admin')
 @section('content')
+<div style="margin-bottom: 10px;" class="row">
+    <div class="col-lg-12">
+        <a class="btn btn100 btn-success" href="{{ route('admin.structures.create') }}">
+            {{ trans('global.add_new') }}
+        </a>
+        <a class="btn btn100 btn-info" href="{{ route('admin.structures.index') }}">
+            {{ trans('global.back_to_list') }}
+        </a>
+    </div>
+</div>
 <div class="card">
     <div class="card-header">
         {{ trans('global.reorder') }} {{ trans('cruds.structure.title_singular') }}
