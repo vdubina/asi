e6202cc438ca3783339ac8536018ac778ca20ab6
Vladimir Dubina <vdubina@zgtec.com>
2021-10-26 16:00:12 +0300

    Fixing bugs

--- resources/views/admin/structures/reorder.blade.php
+++ resources/views/admin/structures/reorder.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div style="margin-bottom: 10px;" class="row">
     <div class="col-lg-12">
-        <a class="btn btn100 btn-success" href="{{ route('admin.structures.create') }}">
-            {{ trans('global.add_new') }}
+        <a class="btn btn100 btn-success" href="javascript://" onclick="$('#toArray').click();">
+            {{ trans('global.save') }}
         </a>
         <a class="btn btn100 btn-info" href="{{ route('admin.structures.index') }}">
             {{ trans('global.back_to_list') }}
